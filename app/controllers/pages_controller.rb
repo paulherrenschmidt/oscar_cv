cb6fa10d0ae8044b18cc15ca8ecc3f15133404f8
Paul HERRENSCHMIDT <herrenschmidtpaul@gmail.com>
2016-09-08 18:45:13 +0200

    oscar cv version1

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,40 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def portfolio
+  end
+
+  def contact
+  end
+
+  def cv
+  end
+
+  def receive
+    page = Page.find_by(address: email.to.first)
+    page.emails.create(
+      subject: email.subject,
+      body: email.body
+    )
+
+    if email.has_attachments?
+      email.attachments.each do |attachment|
+        page.attachments.create({
+          file: attachment,
+          description: email.subject
+        })
+      end
+    end
+  end
+
+   private
+
+  def set_pages
+    @product = Product.find(params[:id])
+  end
+
+  def pages_params
+    params.require(:product).permit(:name, :price, :description, :brand, :ecommerce, :category, :tracking_number, :purchase_date, :photo, :user_id)
+  end
 end
